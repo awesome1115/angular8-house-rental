6f974d3519e2e5dd71ecf515e9c08238065b3379
awesome1115 <arvidjohansson413@gmail.com>
2023-12-08 11:11:15 +0900

    test version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,6 +14,8 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  //
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
